f6df293c56fd780b21bf3bcca39f156ef31190e9
Suyash S <suyash988@gmail.com>
2021-11-05 18:16:31 -0400

    pizza runner ..

--- pizza_runner/file.sql
+++ pizza_runner/file.sql
@@ -1,12 +1,12 @@
--- How many pizzas were ordered?
+-- 1. How many pizzas were ordered?
 SELECT COUNT(*) FROM pizza_runner.customer_orders;
 
 
--- How many unique customer orders were made?
+-- 2. How many unique customer orders were made?
 SELECT COUNT(DISTINCT pizza_id) FROM pizza_runner.customer_orders;
 
 
--- How many successful orders were delivered by each runner?
+-- 3. How many successful orders were delivered by each runner?
 SELECT 
   runner_id,
   SUM (
@@ -20,7 +20,7 @@ GROUP BY 1
 ORDER BY successful_orders DESC
 
 
--- How many of each type of pizza was delivered?
+-- 4. How many of each type of pizza was delivered?
 SELECT 
   pizza_name,
   COUNT(customer_orders.pizza_id) as delivered_pizzas
@@ -32,4 +32,29 @@ JOIN pizza_runner.runner_orders
 WHERE pizza_runner.runner_orders.pickup_time != 'null'
 GROUP BY 1
 
--- How many Vegetarian and Meatlovers were ordered by each customer?
+-- 5. How many Vegetarian and Meatlovers were ordered by each customer?
+SELECT 
+  customer_id, 
+  SUM(CASE WHEN pizza_names.pizza_id = 1 THEN 1 ELSE 0 END) AS meatlovers,
+  SUM(CASE WHEN pizza_names.pizza_id = 2 THEN 1 ELSE 0 END) AS vegetarian
+FROM pizza_runner.customer_orders
+JOIN pizza_runner.pizza_names
+  ON customer_orders.pizza_id = pizza_names.pizza_id
+GROUP BY 1;
+
+-- 6. How many pizzas were delivered that had both exclusions and extras?
+WITH cte_cleaned_customer_orders AS (
+  SELECT
+    order_id,
+    customer_id,
+    pizza_id,
+    CASE WHEN exclusions IN ('null', '') THEN NULL ELSE exclusions END AS exclusions,
+    CASE WHEN extras IN ('null', '') THEN NULL ELSE extras END AS extras,
+    order_time
+  FROM pizza_runner.customer_orders
+)
+SELECT
+  COUNT(*)
+FROM cte_cleaned_customer_orders
+WHERE exclusions IS NOT NULL AND extras IS NOT NULL;
+
